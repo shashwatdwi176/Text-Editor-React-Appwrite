a111e9be70ff346fafb9284223c1624598a9b975
shashwatdwi176 <dwivedishashwat176@gmail.com>
2024-03-03 19:24:04 +0530

    Added Project Setup and Appwrite configuration

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Jai Shree Ram</h1>
+     <h2>Jai Hanuman</h2>
+     <h1>Jai Maa Gayatri</h1>
+    </>
+  )
+}
+
+export default App
