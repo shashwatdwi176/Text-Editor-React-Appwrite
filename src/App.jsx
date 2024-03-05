54623b2e341e7b704ad0f82481cb3e52286d7c1d
shashwatdwi176 <dwivedishashwat176@gmail.com>
2024-03-06 01:43:34 +0530

    Setup redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-
+import React , {useState, useEffect} from "react";
+import { useDispatch } from 'react-redux'
 import './App.css'
-
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import { Footer, Header } from "./components/index";
+import { Outlet } from "react-router-dom";
 function App() {
 
+const [loading , setLoading] = useState(true)
+const dispatch = useDispatch()
 
-  return (
-    <>
-     <h1>Jai Shree Ram</h1>
-     <h2>Jai Hanuman</h2>
-     <h1>Jai Maa Gayatri</h1>
-    </>
-  )
+useEffect(() => {
+  authService.getCurrentUser()
+  .then((userData) => {
+    if (userData){
+      dispatch(login({userData}))
+    }else {
+      dispatch(logout())
+    }
+  })
+  .finally(() => setLoading(false))
+},[])
+  return !loading ? (
+    <div className="min-h-sc flex flex-wrap  content-between bg-gray-400">
+    <div className="w-full block"> 
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  test
+    </div>
+  ) : null
+  
 }
 
 export default App
